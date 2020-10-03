ca5eb919349f94a94f9ebe97077230407b7bfe70
Ricardomeran <Ricardomeran@gmail.com>
2020-10-03 13:59:33 -0400

    Presentacion Angular

--- Primer-ProyectoRM/src/app/app.component.ts
+++ Primer-ProyectoRM/src/app/app.component.ts
@@ -5,7 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Primer-ProyectoRM';
+export class AppComponent {  title = 'Primer-ProyectoRM';
+public nombre: string = 'Ricardo Meran';
+public bio: string = 'Coming soon Front End Angular'
+
 }
 
--- Primer-ProyectoRM/src/app/app.module.ts
+++ Primer-ProyectoRM/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FistComponent } from './fist/fist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FistComponent
   ],
   imports: [
     BrowserModule,
