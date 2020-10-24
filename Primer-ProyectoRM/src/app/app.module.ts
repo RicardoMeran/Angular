0158eede346e95d17b8f3cde799737ed46cbe99d
Ricardomeran <Ricardomeran@gmail.com>
2020-10-24 12:27:51 -0400

    Listo

--- Primer-ProyectoRM/src/app/app.module.ts
+++ Primer-ProyectoRM/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FistComponent } from './fist/fist.component';
 import { PresentacionComponent } from './presentacion/presentacion.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PresentacionComponent } from './presentacion/presentacion.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
